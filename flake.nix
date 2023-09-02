90e70860d2a5ba9220573187fbe482249075c6b3
Benedikt Broich <b.broich@posteo.de>
2023-09-02 16:24:09 +0200

    docs: nixos package

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "CLI to watch Movies/TV Shows from the terminal";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+  with import nixpkgs { system = "${system}"; };
+  let
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    packages.lobster = callPackage ./default.nix { };
+    packages.default = self.packages.${system}.lobster;
+  });
+}
