84f035a70f5535b70bf413bf7144e666051cf43a
NotAShelf <raf@notashelf.dev>
2024-07-11 13:08:21 +0300

    nix: make source path reproducible; modernize & un-shittify derivation
    And remove previous maintainer...

--- default.nix
+++ default.nix
@@ -1,7 +1,8 @@
 {
+  stdenvNoCC,
+  testers,
   coreutils,
   curl,
-  fetchFromGitHub,
   ffmpeg,
   fzf,
   gnugrep,
@@ -12,23 +13,21 @@
   makeWrapper,
   mpv,
   openssl,
-  stdenv,
-  testers,
 }:
-stdenv.mkDerivation (finalAttrs: {
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "lobster";
   version = "4.1.1";
 
-  src = ./.;
-  # src = fetchFromGitHub {
-  #   owner = "justchokingaround";
-  #   repo = "lobster";
-  #   rev = "v${finalAttrs.version}";
-  #   hash = "sha256-YBgZmdi3eIbXlhPzMNi6bGpX/vdxGNcvc1gMx98o354="; # 4.0.6
-  # };
+  src = builtins.path {
+    name = "${finalAttrs.pname}-${finalAttrs.version}";
+    filter = lib.cleanSourceFilter;
+    path = ./.;
+  };
 
-  nativeBuildInputs = [
-    coreutils # wc
+  nativeBuildInputs = [makeWrapper];
+
+  wrapperPaths = lib.makeBinPath [
+    coreutils
     curl
     ffmpeg
     fzf
@@ -36,38 +35,31 @@ stdenv.mkDerivation (finalAttrs: {
     gnupatch
     gnused
     html-xml-utils
-    makeWrapper
     mpv
     openssl
   ];
 
   installPhase = ''
+    runHook preInstall;
     mkdir -p $out/bin
-    cp lobster.sh $out/bin/lobster
+    cp ./lobster.sh $out/bin/lobster
+    runHook postInstall
+  '';
+
+  postInstall = ''
     wrapProgram $out/bin/lobster \
-      --prefix PATH : ${lib.makeBinPath [
-      coreutils
-      curl
-      ffmpeg
-      fzf
-      gnugrep
-      gnupatch
-      gnused
-      html-xml-utils
-      mpv
-      openssl
-    ]}
+      --prefix PATH : $wrapperPaths
   '';
 
   passthru.tests.version = testers.testVersion {
     package = finalAttrs.finalPackage;
   };
 
-  meta = with lib; {
+  meta = {
     description = "CLI to watch Movies/TV Shows from the terminal";
     homepage = "https://github.com/justchokingaround/lobster";
-    license = licenses.gpl3;
-    maintainers = with maintainers; [benediktbroich];
-    platforms = platforms.unix;
+    license = lib.licenses.gpl3;
+    maintainers = with lib.maintainers; [NotAShelf];
+    platforms = lib.platforms.unix;
   };
 })
