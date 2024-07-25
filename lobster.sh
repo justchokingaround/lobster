623d4e78473f8a2c5599e598d93c85052ed05c58
chokerman <44473782+justchokingaround@users.noreply.github.com>
2024-07-25 02:34:41 +0000

    fix: update decryption api to lobster-decryption.netlify

--- lobster.sh
+++ lobster.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-LOBSTER_VERSION="4.2.7"
+LOBSTER_VERSION="4.2.8"
 
 config_file="$HOME/.config/lobster/lobster_config.txt"
 lobster_editor=${VISUAL:-${EDITOR}}
@@ -341,7 +341,7 @@ EOF
         provider_link=$(printf "%s" "$parse_embed" | cut -f1)
         source_id=$(printf "%s" "$parse_embed" | cut -f3)
         embed_type=$(printf "%s" "$parse_embed" | cut -f2)
-        json_data=$(curl -s "http://provider.akash-palmito.org:30174/rabbit/${source_id}")
+        json_data=$(curl -s "https://lobster-decryption.netlify.app/decrypt?id=${source_id}")
         [ -n "$json_data" ] && extract_from_json
     }
 
