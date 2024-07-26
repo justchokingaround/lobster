3cab79a68173cb7aaf00c9f25510dd1bcff170f0
Med Anis Jbara <74317212+medanisjbara@users.noreply.github.com>
2024-07-26 14:47:06 +0100

    fix: remove slashes from filename (#216)

--- lobster.sh
+++ lobster.sh
@@ -578,7 +578,8 @@ EOF
     # download_video [url] [title] [download_dir] [thumbnail_file]
     # TODO: add subtitles
     download_video() {
-        dir="$(printf "%s/%s" "$3" "$2" | tr -d ':')"
+        title="$(printf "%s" "$2" | tr -d ':/')"
+        dir="${3}/${title}"
         ffmpeg -loglevel error -stats -i "$1" -c copy "$dir.mp4"
     }
     choose_from_trending_or_recent() {
