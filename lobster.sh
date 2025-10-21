acd5b754ec3893df0b4e71b2f13e8940724b9f89
Ibrahim Ahmed <13389823+ibz096@users.noreply.github.com>
2025-10-21 01:08:31 -0400

    Added: Subtitle support to VLC-iOS/iSH

--- lobster.sh
+++ lobster.sh
@@ -822,7 +822,9 @@ EOF
                 ;;
             mpv_android) nohup am start --user 0 -a android.intent.action.VIEW -d "$video_link" -n is.xyz.mpv/.MPVActivity -e "title" "$displayed_title" >/dev/null 2>&1 & ;;
             iSH)
-                printf "\e]8;;vlc://%s\a~~~~~~~~~~~~~~~~~~~~\n~ Tap to open VLC ~\n~~~~~~~~~~~~~~~~~~~~\e]8;;\a\n" "$video_link"
+                # Only supports single subtitle file, as I don't know how to pass multiple subtitles to VLC-iOS via iSH. If subtitles are null, video should still be played.
+                # If multiple subtitles are found, subtitles will fail to load in VLC-iOS.
+                printf "\e]8;;vlc-x-callback://x-callback-url/stream?url=%s&sub=%s\a~ Tap to open VLC ~\e]8;;\a\n" "$video_link" "$subs_links"
                 sleep 5
                 ;;
             *yncpla*) nohup "syncplay" "$video_link" -- --force-media-title="${displayed_title}" >/dev/null 2>&1 & ;;
